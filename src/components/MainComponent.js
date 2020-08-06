a63303584df7c204cb52206089e0616cbca9c179
Pettone <60547369+Pettone@users.noreply.github.com>
2020-08-06 12:29:11 -0400

    React Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
 import DishDetail from './DishdetailComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
     constructor(props) {
       super(props);
       this.state = {
         dishes: DISHES,
-        selectedDish: null
       };
     }
 
@@ -29,20 +30,23 @@ class Main extends Component {
         }
     }
 
-    onDishSelect(dishId) {
-        this.setState({ selectedDish: dishId });
+  render(){
+
+    const HomePage = () => {
+      return(
+        <Home />
+      );
     }
 
-  render(){
     return (
     <div>
       <Header />
-      <Menu dishes={this.state.dishes} 
-        onClick={(dishId) => this.onDishSelect(dishId)}/>
-        {this.renderDish(this.state.dishes.filter(
-          (dish) => dish.id  === this.state.selectedDish )[0]  
-        )}
-        <Footer />
+      <Switch>
+        <Route path="/home" component={HomePage} />
+        <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+        <Redirect to="/home" /> 
+      </Switch>
+      <Footer />
     </div>
     );
   }
@@ -50,3 +54,9 @@ class Main extends Component {
 }
 
 export default Main;
+
+/*
+{this.renderDish(this.state.dishes.filter(
+          (dish) => dish.id  === this.state.selectedDish )[0]  
+        )}
+*/
