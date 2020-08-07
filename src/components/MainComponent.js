290705c22d39f36fd697a7add2d89a411269f2be
Pettone <60547369+Pettone@users.noreply.github.com>
2020-08-06 21:10:37 -0400

    Single Page Application Part I

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,7 +3,11 @@ import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import DishDetail from './DishdetailComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
@@ -12,6 +16,9 @@ class Main extends Component {
       super(props);
       this.state = {
         dishes: DISHES,
+        comments: COMMENTS,
+        leaders: LEADERS,
+        promotions: PROMOTIONS
       };
     }
 
@@ -34,7 +41,10 @@ class Main extends Component {
 
     const HomePage = () => {
       return(
-        <Home />
+        <Home dish={this.state.dishes.filter( (dish) => dish.featured )[0]}
+          promotions={this.state.promotions.filter( (promo) => promo.featured )[0]}
+          leader={this.state.leaders.filter( (leader) => leader.featured )[0]}
+        />
       );
     }
 
@@ -44,6 +54,7 @@ class Main extends Component {
       <Switch>
         <Route path="/home" component={HomePage} />
         <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+        <Route exact path='/contactus' component={Contact} />
         <Redirect to="/home" /> 
       </Switch>
       <Footer />
